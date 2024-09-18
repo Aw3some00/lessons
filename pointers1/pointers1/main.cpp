bb89386e5202200f2c94cc550628e1920ff87fae
Rashidov Fuad <rashidovfuad@Fuad.local>
2024-09-18 11:44:01 +0400

    update

---
+++ pointers1/pointers1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+struct Point{
+    double x,y,z;
+};
+
+int main() {
+  /*  int x=42;
+    int y=20;
+    int*ptr;
+    ptr=nullptr;
+    ptr=&x;
+    std::cout<<*ptr<<"\n";
+    ptr=&y;
+    std::cout<<*ptr<<"\n";*/
+    Point p={3.0,4.0,5.0};
+    Point*ptr=&p;
+    std::cout<<(*ptr).x<<"\n";
+    std::cout<<ptr->x<<"\n";
+    
+    
+    
+    
+
+    return 0;
+}
